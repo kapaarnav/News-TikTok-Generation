56b6162763001851a61d9862e0eae22e8f398da5
0cchang <calebschang13@gmail.com>
2025-01-25 02:52:35 -0500

    calebs test paraphrasing

--- scraping.py
+++ scraping.py
@@ -12,29 +12,40 @@ CHROME_DRIVER_PATH = "/Users/aarnavkapoor/Downloads/chromedriver-mac-arm64/chrom
 service = Service(CHROME_DRIVER_PATH)
 driver = webdriver.Chrome(service=service)
 
+# array that holds relavant links
 whitehouse = ["https://www.whitehouse.gov/briefings-statements/","https://www.whitehouse.gov/fact-sheets/"]
-frontPageLinks = []
+frontPageLinks = [] # storage for all the sublinks we will get
+
+# standard try for selenium driver
 try:
+    # for each link that we scrape
     for wh in whitehouse: 
-        driver.get(wh)
+        driver.get(wh) # open the site
 
-        time.sleep(3)
+        time.sleep(3) # wait for it to load
 
+        # get all js objects that are this css selector, found by inspecting
         links = driver.find_elements(By.CSS_SELECTOR, "h2.wp-block-post-title a")
     
+        # each link has a href for the site we actually want
         for link in links:
             l = link.get_attribute("href") 
             if l:
-                frontPageLinks.append(l)
+                frontPageLinks.append(l) # store it
 
-        allText = []
-    
+        allText = [] # storage for the text on each site
+
+        # navigate each sublink and extract text
         for link in frontPageLinks:
             try:
-                driver.get(link)  # Replace with your target URL
-                time.sleep(2)
+                driver.get(link)  # launch site
+
+                time.sleep(2) # wait for site to load
                 
+                # get all the text
                 page_text = driver.find_element(By.TAG_NAME, "body").text
+
+                # store it
                 allText.append(page_text)
             except Exception as e:
                 print(f"Error visiting link {link}: {e}")
@@ -43,18 +54,25 @@ finally:
     # Close the WebDriver
     driver.quit()
 
-titles = []
-dates =  []
+titles = [] # store all titles
+dates =  [] # date etc
 texts = []
 for p in allText:
-    c = p.split("\n")
-    datePointer = 8
+    c = p.split("\n") # split the text into lines
+    datePointer = 8 # based on formatting of the site, 
+    # 8 is usually the index of where the date is but we scoot it forward until we find it
+    # january because we are in january, can be futureproofed by using a hashmap and time modele
     while not "January" in c[datePointer]:
         datePointer += 1
+    
+    #everything before the date is the title
     titles.append(" ".join(c[7:datePointer]))
+    #add the date ptointer
     dates.append(c[datePointer])
+    # everything after is the article body
     texts.append(" ".join(c[9:-12]))
 
+# make the dataframe from those columns and keep the original link for citations
 data = zip(frontPageLinks, titles, dates, texts)
 df = pd.DataFrame(data, columns=["link", "title", "date", "text"])
 
